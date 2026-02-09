744dde57d37505cb833d52de43a2d25c44636d95
Zedric <cedricvhonpidlaoan@gmail.com>
2026-02-09 09:19:28 +0800

    small changes

--- front-end/src/components/Layout/StudentLayout.tsx
+++ front-end/src/components/Layout/StudentLayout.tsx
@@ -3,6 +3,7 @@ import Sidebar from '../common/Sidebar';
 import type { NavItem } from '../interface/type';
 import { CalendarClock, FileText, LayoutDashboard } from 'lucide-react';
 import { Toaster } from '@/components/ui/sonner';
+import UserNav from '../common/UserNav';
 
 const StundentLayout = () => {
 	const navItem: NavItem[] = [
@@ -23,11 +24,14 @@ const StundentLayout = () => {
 		},
 	];
 	return (
-		<div className="flex gap-5 content-start h-full relative">
+		<div className="flex content-start h-full relative">
 			<Sidebar navItem={navItem} />
-			<main className="px-4 py-5 bg-background grow relative">
-				<Outlet />
-				<Toaster position="top-center" />
+			<main className="w-full">
+				<UserNav />
+				<div className="px-10 py-5 bg-background grow relative">
+					<Outlet />
+					<Toaster position="top-center" />
+				</div>
 			</main>
 		</div>
 	);
---
+++ front-end/src/components/common/UserNav.tsx
@@ -0,0 +1,9 @@
+const UserNav = () => {
+	return (
+		<>
+			<div className="border-b border-muted w-full p-2 transition-all duration-300 flex justify-between h-16 items-center px-10"></div>
+		</>
+	);
+};
+
+export default UserNav;
